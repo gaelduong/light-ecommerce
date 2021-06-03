012d8b59bdf0d176b8f9e6c6e756c26a2ddb84ad
Gael Duong <mr_jupipro@yahoo.com>
2021-06-04 01:48:28 +0700

    Client send add product request to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
 import "./App.css";
+const serverUrl = "http://localhost:5000";
 
 const App = () => {
     const [products, setProducts] = useState([]);
@@ -10,15 +10,22 @@ const App = () => {
         const fetchProducts = async () => {
             // console.log(process.env.NODE_ENV);
             // const serverUrl = process.env.REACT_APP_API_BASE_URL ? process.env.REACT_APP_API_BASE_URL : "http://localhost:5000";
-            const serverUrl = "http://localhost:5000";
             const { data } = await axios.get(`${serverUrl}/products`);
             setProducts(data);
         };
         fetchProducts();
     }, []);
 
+    const addProductHandler = async () => {
+        const { data } = await axios.post(`${serverUrl}/products`, {
+            name: "Watermelon"
+        });
+        console.log(data);
+    };
+
     return (
         <div className="App">
+            <button onClick={addProductHandler}> Add product</button>
             <h2>My products: </h2>
             {products.map((product) => {
                 return <p>{product.name}</p>;
--- server/server.js
+++ server/server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+app.use(express.json());
 app.use(cors());
 
 const products = [{ name: "Apple" }, { name: "Orange" }, { name: "Banana" }];
 
 app.get("/products", (req, res) => {
     res.json(products);
+});
+
+app.post("/products", (req, res) => {
+    console.log(req.body);
+    res.send("Success");
 });
 
 const PORT = process.env.PORT || 5000;
