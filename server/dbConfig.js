112a8e2c46a6a7b1dd6daa9ee40f6e2e7a5e1987
Gael Duong <mr_jupipro@yahoo.com>
2021-06-04 17:57:10 +0700

    Basic write, read db operations

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,12 +1,39 @@
 const mongoose = require("mongoose");
 
-const dbConnection = () => {
+const dbConnection = async () => {
     mongoose.connect(process.env.DB_CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    const db = mongoose.connection;
-    db.on("error", console.error.bind(console, "Connection error:"));
-    db.once("open", () => {
+    const connection = mongoose.connection;
+    connection.on("error", console.error.bind(console, "Connection error:"));
+    connection.once("open", () => {
         console.log("Connected to MongoDB");
     });
+
+    // Product schema
+    const productSchema = new mongoose.Schema({
+        name: String
+    });
+
+    // Product model
+    const Product = mongoose.model("products", productSchema);
+
+    // Create a new record
+    const product1 = new Product({ name: "Keyboard" });
+
+    // Insert records
+    try {
+        await product1.save();
+        console.log("Saved");
+    } catch (e) {
+        console.log(e);
+    }
+
+    // Read records
+    try {
+        const products = await Product.find({});
+        console.log(products);
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 module.exports = {
