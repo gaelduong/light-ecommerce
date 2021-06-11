9e2bfae66e581fd05693f9dc4af40a1f0860bafb
Gael Duong <mr_jupipro@yahoo.com>
2021-06-11 10:20:55 +0700

    Separate public and private routes

--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ const cors = require("cors");
 require("dotenv").config();
 const dbConnect = require("./dbConfig");
 const { Product } = require("./models/");
+const bcrypt = require("bcrypt");
 app.use(express.json());
 app.use(cors());
 
 dbConnect();
 
+// Public routes
 app.get("/products", async (req, res) => {
     // Retrieve products from DB
     try {
@@ -21,7 +23,20 @@ app.get("/products", async (req, res) => {
     }
 });
 
-app.post("/products", async (req, res) => {
+// Private routes
+app.get("/products_admin", async (req, res) => {
+    // Retrieve products from DB
+    try {
+        const products = await Product.find({});
+        console.log("Retrieved successfully");
+        res.json(products);
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+    }
+});
+
+app.post("/products_admin", async (req, res) => {
     const product = req.body;
     const newProduct = new Product(product);
     // Insert product to DB
@@ -35,7 +50,7 @@ app.post("/products", async (req, res) => {
     }
 });
 
-app.delete("/products/:id", async (req, res) => {
+app.delete("/products_admin/:id", async (req, res) => {
     try {
         const removedProduct = await Product.findById(req.params.id);
         removedProduct.remove();
@@ -47,7 +62,7 @@ app.delete("/products/:id", async (req, res) => {
     }
 });
 
-app.put("/products/:id", async (req, res) => {
+app.put("/products_admin/:id", async (req, res) => {
     try {
         const { name } = req.body;
         const productToUpdate = await Product.findById(req.params.id);
