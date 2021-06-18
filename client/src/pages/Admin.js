984fb5baae09433308a992b16e4bc5abeedc2bd0
Gael Duong <mr_jupipro@yahoo.com>
2021-06-18 17:41:31 +0700

    Refactor AddProduct (continued)

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { serverUrl } from "../config";
 import EditProduct from "./EditProduct.js";
+import AddProduct from "./AddProduct.js";
 
 const Admin = ({ history }) => {
    // Loading
@@ -12,16 +13,6 @@ const Admin = ({ history }) => {
 
    // Products changed
    const [productsChanged, setProductsChanged] = useState(true);
-
-   // Product fields - Add
-   const [productNameInput, setProductNameInput] = useState("");
-   const [productPriceInput, setProductPriceInput] = useState(0);
-   const [productDescriptionInput, setProductDescriptionInput] = useState("");
-   const [productCategoryInput, setProductCategoryInput] = useState("");
-   const [productIsInStockInput, setProductIsInStockInput] = useState(true);
-   const [imagePathInput, setImagePathInput] = useState(null);
-   const [imageFile, setImageFile] = useState(null);
-   const [inputFileKey, setInputFileKey] = useState("");
 
    // Product edit
    const [productEditId, setProductEditId] = useState("");
@@ -68,47 +59,6 @@ const Admin = ({ history }) => {
       fetchProducts();
    }, [productsChanged]);
 
-   const handleAddProduct = async (e) => {
-      e.preventDefault();
-
-      if (imageFile) {
-         // Upload image to server
-         const formData = new FormData();
-         formData.append("image", imageFile);
-         try {
-            const { data } = await axios.post(`${serverUrl}/imageupload`, formData, {
-               headers: {
-                  "Content-Type": "multipart/form-data"
-               }
-            });
-            setImagePathInput(data);
-         } catch (e) {
-            return console.log(e);
-         }
-      }
-
-      // Send POST request to add new product to DB
-      try {
-         await axios.post(`${serverUrl}/products_admin`, {
-            name: productNameInput,
-            price: productPriceInput,
-            description: productDescriptionInput,
-            category: productCategoryInput,
-            isInStock: productIsInStockInput,
-            image: imagePathInput
-         });
-         setProductsChanged(true);
-         setProductNameInput("");
-         setProductPriceInput(0);
-         setProductDescriptionInput("");
-         setProductCategoryInput("");
-         setImageFile("");
-         setInputFileKey(Date.now());
-      } catch (e) {
-         return console.log(e);
-      }
-   };
-
    const handleDeleteProduct = async (e, id) => {
       const { data } = await axios.delete(`${serverUrl}/products_admin/${id}`);
       const newProducts = [...products].filter((product) => product._id !== data._id);
@@ -123,74 +73,9 @@ const Admin = ({ history }) => {
 
    return (
       <div className="Admin">
-         <h1> ADD PRODUCT </h1>
          <button onClick={logoutHandler}> Log out </button>
-         <form onSubmit={handleAddProduct}>
-            <label>
-               Name:
-               <input
-                  required
-                  type="text"
-                  value={productNameInput}
-                  onChange={(e) => {
-                     setProductNameInput(e.target.value);
-                  }}
-               />
-            </label>
-            <label>
-               Price:
-               <input
-                  required
-                  type="number"
-                  step={0.01}
-                  min={0}
-                  value={productPriceInput}
-                  onChange={(e) => {
-                     setProductPriceInput(Number(e.target.value).toString());
-                  }}
-               />
-            </label>
 
-            <label>
-               Description:
-               <textarea
-                  value={productDescriptionInput}
-                  onChange={(e) => {
-                     setProductDescriptionInput(e.target.value);
-                  }}
-               />
-            </label>
-
-            <label>
-               Category:
-               <select required value={productCategoryInput} onChange={(e) => setProductCategoryInput(e.target.value)}>
-                  <option value="">Choose category</option>
-                  <option value="categoryA">Category A</option>
-                  <option value="categoryB">Category B</option>
-                  <option value="categoryC">Category C</option>
-                  <option value="categoryD">Category D</option>
-               </select>
-            </label>
-
-            <label>
-               Is in stock:
-               <input
-                  type="checkbox"
-                  checked={productIsInStockInput}
-                  onChange={(e) => {
-                     setProductIsInStockInput(!productIsInStockInput);
-                  }}
-               />
-            </label>
-            <label>
-               Image:
-               <input type="file" name="image" key={inputFileKey} onChange={(e) => setImageFile(e.target.files[0])} />
-            </label>
-
-            <br />
-
-            <input type="submit" value="Add product" />
-         </form>
+         <AddProduct setProductsChanged={setProductsChanged} />
 
          {productEditId && <EditProduct products={products} productEditId={productEditId} setProductEditId={setProductEditId} setProductsChanged={setProductsChanged} />}
 
