6c6deba345888f09fd453fe4581e71ae22ead7e4
Gael Duong <mr_jupipro@yahoo.com>
2021-06-18 14:41:06 +0700

    Remove login link

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,6 @@ const App = () => {
                   <li>
                      <Link to="/admin">Admin</Link>
                   </li>
-                  <li>
-                     <Link to="/login">Login</Link>
-                  </li>
                </ul>
             </nav>
             <Switch>
