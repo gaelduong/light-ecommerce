99edce6bc6448e4c04662b1a02476e73fffad37a
Gael Duong <mr_jupipro@yahoo.com>
2021-07-01 03:31:45 +0700

    Display prices based on selected variations

--- client/src/components/User/ProductDetails.js
+++ client/src/components/User/ProductDetails.js
@@ -4,16 +4,42 @@ import axios from "axios";
 import { apiUrl } from "../../config";
 import placeholderImage from "../../assets/placeholder-image-2.png";
 
+const priceEx = {
+   singlePrice: 12,
+   multiplePrices: {
+      variations: [
+         { name: "color", values: ["red", "blue"] },
+         { name: "size", values: ["L", "XL"] }
+      ],
+      variationPriceList: [
+         { color: "red", size: "L", price: 11 },
+         { color: "red", size: "XL", price: 20 },
+         { color: "blue", size: "L", price: 14 },
+         { color: "blue", size: "XL", price: 25 }
+      ]
+   }
+};
+
 const ProductDetails = () => {
    const [product, setProduct] = useState(null);
+   const [variationSelection, setVariationSelection] = useState({});
+   const [displayPrice, setDisplayPrice] = useState(0);
+
    const { productId } = useParams();
 
    useEffect(() => {
       const fetchProduct = async () => {
          try {
-            console.log(productId);
             const { data } = await axios.get(`${apiUrl}/products/${productId}`);
             console.log("🚀 ~ file: ProductDetails.js ~ line 15 ~ fetchProduct ~ data", data);
+            // if(data.price.singlePrice){
+            //      return setDisplayPrice(data.price.singlePrice)
+            // } else
+            const defaultPrice = priceEx.multiplePrices.variationPriceList[0].price;
+            setDisplayPrice(defaultPrice);
+            // Default variation selection
+            const variationSelection = priceEx.multiplePrices.variations.reduce((obj, item) => Object.assign(obj, { [item.name]: item.values[0] }), {});
+            setVariationSelection(variationSelection);
             setProduct(data);
          } catch (error) {
             console.error(error);
@@ -22,42 +48,45 @@ const ProductDetails = () => {
       fetchProduct();
    }, [productId]);
 
-   if (!product) return <></>;
+   useEffect(() => {
+      if (!product) return;
+      if (!priceEx.multiplePrices) return;
+      // Get the corresponding price based on selected varations
+      const variationPriceList = priceEx.multiplePrices.variationPriceList;
+      const currentVariationPrice = variationPriceList.find((variationPrice) => {
+         return Object.entries(variationPrice).every(([prop, value]) => value === variationSelection[prop] || prop === "price");
+      });
 
-   const price = {
-      single_price: 12,
-      price_variations: {
-         variations: [
-            {
-               name: "color",
-               values: ["red", "blue"]
-            },
-            {
-               name: "size",
-               values: ["L", "XL"]
-            }
-         ],
-         prices: [
-            { color: "red", size: "L", price: 10 },
-            { color: "red", size: "XL", price: 20 },
-            { color: "blue", size: "L", price: 10 },
-            { color: "blue", size: "XL", price: 20 }
-         ]
-      }
+      setDisplayPrice(currentVariationPrice.price);
+   }, [product, variationSelection]);
+
+   const handleSelection = (e) => {
+      setVariationSelection({
+         ...variationSelection,
+         [e.target.name]: e.target.value
+      });
    };
 
-   const priceVariations = price.price_variations;
-   const variationsElems = !priceVariations
-      ? ""
-      : priceVariations.variations.map(({ name, values }) => (
-           <div>
-              <p> {name}</p>
-              {values.map((value) => (
-                 <button> {value}</button>
-              ))}
-           </div>
-        ));
+   const variationsElems =
+      priceEx.multiplePrices &&
+      priceEx.multiplePrices.variations.map(({ name, values }) => (
+         <div key={name}>
+            <p> {name}</p>
+            {values.map((value) => (
+               <button
+                  key={value}
+                  name={name}
+                  value={value}
+                  style={{ display: "inline", margin: "0 5px 0 0", background: variationSelection && variationSelection[name] === value ? "yellowgreen" : "" }}
+                  onClick={handleSelection}
+               >
+                  {value}
+               </button>
+            ))}
+         </div>
+      ));
 
+   if (!product) return <></>;
    return (
       <React.Fragment>
          <Link to={`/products`}>
@@ -69,12 +98,11 @@ const ProductDetails = () => {
             <img className="img-display" src={placeholderImage} alt=" product" />
          )}
          <p>Name: {product.name}</p>
-         <p>Price:{product.price}</p>
+         <p>Price: {displayPrice}</p>
+         {variationsElems}
          <p>Description: {product.description}</p>
          <p>Category: {product.category}</p>
          <p>{product.isInStock ? "(In Stock)" : "(Out Of Stock)"}</p>
-
-         {variationsElems}
       </React.Fragment>
    );
 };
