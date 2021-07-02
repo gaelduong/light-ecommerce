3bfb98984dd9f9f74da3132c9e96618dff58ef88
Gael Duong <mr_jupipro@yahoo.com>
2021-07-02 14:28:25 +0700

    Implement price variations

--- client/src/components/Admin/ProductAdd.js
+++ client/src/components/Admin/ProductAdd.js
@@ -6,6 +6,14 @@ import AdminContainer from "./AdminContainer.js";
 import placeholderImage from "../../assets/placeholder-image.png";
 import { ImageInputList } from "../Common";
 
+const generateRandomKey = () => {
+   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+      var r = (Math.random() * 16) | 0,
+         v = c === "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+   });
+};
+
 const ProductAdd = ({ history }) => {
    const authVerified = useVerifyAuth(history);
 
@@ -18,6 +26,8 @@ const ProductAdd = ({ history }) => {
    });
 
    const [imagesInput, setImagesInput] = useState([...Array(6)].map((_, idx) => ({ order: idx, imageFile: null, inputKey: "", imageDisplay: placeholderImage })));
+
+   const [variations, setVariations] = useState([]);
 
    const handleChange = (e) => {
       let value = e.target.value;
@@ -35,6 +45,50 @@ const ProductAdd = ({ history }) => {
       };
 
       setProductFields(newProductFields);
+   };
+
+   const handleTurnOffVariations = () => {
+      setVariations([]);
+   };
+
+   const handleCreateVariation = () => {
+      const newVariation = { variationName: "", options: [{ optionId: generateRandomKey(), value: "" }], id: generateRandomKey() };
+      setVariations([...variations, newVariation]);
+   };
+
+   const handleAddVariationOption = (id) => {
+      const newVariations = variations.map((variation) => {
+         if (variation.id === id) {
+            variation.options.push({ optionId: generateRandomKey(), value: "" });
+         }
+         return variation;
+      });
+      setVariations(newVariations);
+   };
+
+   const handleVariationNameChange = (e, idx) => {
+      const newVariations = [...variations];
+      newVariations[idx].variationName = e.target.value;
+      console.log("🚀 ~ file: ProductAdd.js ~ line 50 ~ handleVariationNameChange ~ newVariations", newVariations);
+      setVariations(newVariations);
+   };
+
+   const handleVariationOptionChange = (e, variationIdx, idx) => {
+      const newVariations = [...variations];
+      newVariations[variationIdx].options[idx].value = e.target.value;
+      setVariations(newVariations);
+   };
+
+   const handleDeleteVariationOption = (variationIdx, optionId) => {
+      const newVariations = [...variations];
+      newVariations[variationIdx].options = newVariations[variationIdx].options.filter((option) => option.optionId !== optionId);
+      setVariations(newVariations);
+   };
+
+   const handleDeleteVariation = (id) => {
+      const newVariations = [...variations].filter((variation) => variation.id !== id);
+      console.log("🚀 ~ file: ProductAdd.js ~ line 90 ~ handleDeleteVariation ~ newVariations", newVariations);
+      setVariations(newVariations);
    };
 
    const handleAddProduct = async (e) => {
@@ -59,15 +113,32 @@ const ProductAdd = ({ history }) => {
          }));
 
          console.log("🚀 ~ file: ProductAdd.js ~ line 94 ~ handleAddProduct ~ data", data);
+
+         const priceEx = {
+            // singlePrice: 12
+            multiplePrices: {
+               variations: [
+                  { name: "storage", values: ["128gb", "256gb"] },
+                  { name: "ram", values: ["16gb", "32gb"] }
+               ],
+               variationPriceList: [
+                  { options: ["128gb", "16gb"], price: 11 },
+                  { options: ["128gb", "32gb"], price: 20 },
+                  { options: ["256gb", "16gb"], price: 14 },
+                  { options: ["256gb", "32gb"], price: 25 }
+               ]
+            }
+         };
          // Send POST request to add new product to DB
          await axios.post(`${apiUrl}/products_admin`, {
             name: productFields.name,
-            price: productFields.price,
+            price: priceEx,
             description: productFields.desc,
             category: productFields.category,
             isInStock: productFields.isInStock,
             images: imageOrderPathList
          });
+
          setTimeout(() => {
             history.push("/admin");
          }, 200);
@@ -78,6 +149,97 @@ const ProductAdd = ({ history }) => {
 
    if (!authVerified) return <></>;
 
+   const variationForm = (
+      <div>
+         <button type="button" onClick={handleTurnOffVariations}>
+            Turn off variations
+         </button>
+         {variations.map((variation, idx) => (
+            <div key={variation.id}>
+               <button style={{ display: "inline" }} onClick={() => handleDeleteVariation(variation.id)}>
+                  x
+               </button>
+               <label>
+                  {`Variation ${idx + 1}`}:
+                  <input
+                     required
+                     type="text"
+                     placeholder="Variation name: size, color, etc."
+                     value={variations[idx].variationName}
+                     onChange={(e) => handleVariationNameChange(e, idx)}
+                  />
+                  Options:
+                  {variation.options.map((option, idx2) => (
+                     <div key={option.optionId}>
+                        <input
+                           required
+                           type="text"
+                           placeholder="Variation option: red, large, etc."
+                           value={variation.options[idx2].value}
+                           onChange={(e) => handleVariationOptionChange(e, idx, idx2)}
+                        />
+                        {idx2 >= 1 && (
+                           <button type="button" onClick={() => handleDeleteVariationOption(idx, option.optionId)}>
+                              x
+                           </button>
+                        )}
+                     </div>
+                  ))}
+               </label>
+               <button type="button" onClick={() => handleAddVariationOption(variation.id)}>
+                  + Add options
+               </button>
+               <hr />
+            </div>
+         ))}
+         <button onClick={handleCreateVariation} type="button">
+            Add more variation
+         </button>
+
+         <p> Variation list </p>
+         <table>
+            <tbody>
+               <tr>
+                  <th>{variations[0]?.variationName || "Variation name"}</th>
+                  {variations[1] && <th>{(variations[1] && variations[1].variationName) || "Variation name"}</th>}
+                  <th>Price</th>
+               </tr>
+               {variations[0]?.options?.length > 0 &&
+                  variations[0].options.map(({ value }) => (
+                     <tr key={generateRandomKey()}>
+                        <td>{value || "Option"} </td>
+
+                        {variations[1] && (
+                           <td>{variations[1].options && variations[1].options.map(({ value }) => <div key={generateRandomKey()}>{value || "Option"}</div>)}</td>
+                        )}
+                        <td>
+                           {variations[1]?.options?.length > 0 ? (
+                              variations[1].options.map((option) => (
+                                 <input
+                                    key={generateRandomKey()}
+                                    // value={0}
+                                    // onChange={() => {
+                                    //    console.log("a");
+                                    // }}
+                                 />
+                              ))
+                           ) : (
+                              <input
+                                 key={generateRandomKey()}
+                                 // value={0}
+                                 // onChange={() => {
+                                 //    console.log("a");
+                                 // }}
+                              />
+                           )}
+                        </td>
+                     </tr>
+                  ))}
+            </tbody>
+         </table>
+      </div>
+   );
+
    const { name, price, desc, category, isInStock } = productFields;
    return (
       <AdminContainer history={history}>
@@ -87,13 +249,14 @@ const ProductAdd = ({ history }) => {
                * Name:
                <input required type="text" name="name" value={name} onChange={handleChange} />
             </label>
-            <label>
-               * Price:
-               <input required type="number" name="price" step={0.1} min={0} value={price} onChange={handleChange} />
-            </label>
-            <label> OR </label>
-            <input required type="button" name="price" value="Price with variations" />
-            <label> </label>
+            {variations.length === 0 && (
+               <label>
+                  * Price:
+                  <input required type="number" name="price" step={0.1} min={0} value={price} onChange={handleChange} />
+               </label>
+            )}
+            {variations.length === 0 && <input required type="button" name="price" value="Enable variations" onClick={handleCreateVariation} />}
+            {variations.length > 0 && variationForm}
             <label>
                Description:
                <textarea name="desc" value={desc} onChange={handleChange} />
