644350e72991710406cd2db2039ef1d63cfad967
Gael Duong <mr_jupipro@yahoo.com>
2021-07-04 13:04:17 +0700

    Remove unused code

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const dbConnect = require("./dbConfig");
-const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 app.use(cors());
@@ -14,17 +13,6 @@ const publicRoutes = require("./routes/publicRoutes.js");
 const adminRoutes = require("./routes/adminRoutes.js");
 const authRoutes = require("./routes/authRoutes.js");
 const imageUploadRoutes = require("./routes/imageUploadRoutes.js");
-
-const authMiddleware = async (req, res, next) => {
-   try {
-      const accessToken = req.headers["authorization"].split(" ")[1];
-      const decoded = jwt.verify(accessToken, process.env.JWT_ACCESS_TOKEN);
-      // res.status(200).json(decoded);
-      if (decoded) next();
-   } catch (e) {
-      res.sendStatus(403);
-   }
-};
 
 app.use("/api", publicRoutes);
 app.use("/api", adminRoutes);
