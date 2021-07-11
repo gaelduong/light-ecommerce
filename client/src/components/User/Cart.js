16163935af822f4de8c43901bc8e64154e50badb
Gael Duong <mr_jupipro@yahoo.com>
2021-07-11 11:18:47 +0700

    Show as table in cart

--- client/src/components/User/Cart.js
+++ client/src/components/User/Cart.js
@@ -11,31 +11,38 @@ const Cart = () => {
    return (
       <div>
          <h3> Cart </h3>
-         {cart.items.map(({ product, quantity, subTotal }) => (
-            <div key={product.productId + JSON.stringify(product.variation)}>
-               <span>
-                  <img className="img-display" src={product.image} alt="product" />
-               </span>
-               <span> {product.name}</span>
-               <span> Subtotal: {subTotal}</span>
-               <span> {Object.entries(product.variation).map((v) => v + " ")}</span>
-               <span> Quantity: </span>
-               <span>
-                  <button
-                     className="inline"
-                     onClick={() => {
-                        if (quantity - 1 === 0) {
-                           return dispatch(deleteCartItem({ productId: product.productId, productVariation: product.variation }));
-                        }
-                        dispatch(
-                           updateCartItemQuantity({ productId: product.productId, productVariation: product.variation, quantity: quantity - 1 })
-                        );
-                     }}
-                  >
-                     -
-                  </button>
-                  <span> {quantity} </span>
-                  {/* <input
+         <table>
+            <tbody>
+               {cart.items.map(({ product, quantity, subTotal }) => (
+                  <tr key={product.productId + JSON.stringify(product.variation)}>
+                     <td>
+                        <img className="img-display" src={product.image} alt="product" />
+                     </td>
+                     <td> {product.name}</td>
+                     <td>
+                        {Object.entries(product.variation).map(([variationName, value]) => (
+                           <div>
+                              {variationName}: {value}
+                           </div>
+                        ))}
+                     </td>
+                     <td> Quantity: </td>
+                     <td>
+                        <button
+                           className="inline"
+                           onClick={() => {
+                              if (quantity - 1 === 0) {
+                                 return dispatch(deleteCartItem({ productId: product.productId, productVariation: product.variation }));
+                              }
+                              dispatch(
+                                 updateCartItemQuantity({ productId: product.productId, productVariation: product.variation, quantity: quantity - 1 })
+                              );
+                           }}
+                        >
+                           -
+                        </button>
+                        <span> {quantity} </span>
+                        {/* <input
                      style={{ width: "32px", textAlign: "center" }}
                      className="inline"
                      type="tel"
@@ -52,27 +59,31 @@ const Cart = () => {
                         )
                      }
                   /> */}
-                  <button
-                     className="inline"
-                     onClick={() =>
-                        dispatch(
-                           updateCartItemQuantity({ productId: product.productId, productVariation: product.variation, quantity: quantity + 1 })
-                        )
-                     }
-                  >
-                     +
-                  </button>
-               </span>
-               &ensp;
-               <button
-                  className="inline"
-                  onClick={() => dispatch(deleteCartItem({ productId: product.productId, productVariation: product.variation }))}
-               >
-                  Delete
-               </button>
-            </div>
-         ))}
-         <div>Total: {cart.total}</div>
+                        <button
+                           className="inline"
+                           onClick={() =>
+                              dispatch(
+                                 updateCartItemQuantity({ productId: product.productId, productVariation: product.variation, quantity: quantity + 1 })
+                              )
+                           }
+                        >
+                           +
+                        </button>
+                     </td>
+                     <td> {subTotal} USD </td>
+                     <td>
+                        <button
+                           className="inline"
+                           onClick={() => dispatch(deleteCartItem({ productId: product.productId, productVariation: product.variation }))}
+                        >
+                           ❌
+                        </button>
+                     </td>
+                  </tr>
+               ))}
+            </tbody>
+         </table>
+         <div>Total: {cart.total} USD </div>
          <button> Checkout (COD) </button>
       </div>
    );
